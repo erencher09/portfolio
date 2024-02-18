b5b1372d9ba89991461062a599e44d35e3225eb2
erencher09 <erencher09@gmail.com>
2024-02-18 16:32:41 -0700

    modified contact information

--- evanportfolioreact/my-portfolio/src/components/Contact.js
+++ evanportfolioreact/my-portfolio/src/components/Contact.js
@@ -47,8 +47,7 @@ export default function Contact() {
                 ADDRESS
               </h2>
               <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
+                Englewood, CO 80112
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -56,12 +55,12 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
+                erencher09@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">970-290-4908</p>
             </div>
           </div>
         </div>
