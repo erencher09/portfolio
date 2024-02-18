cadeb4ad88f11e46ffbb85d93245d952728588c5
erencher09 <erencher09@gmail.com>
2024-02-18 16:03:49 -0700

    created components files

--- evanportfolioreact/my-portfolio/src/App.js
+++ evanportfolioreact/my-portfolio/src/App.js
@@ -1,5 +1,16 @@
+// src/App.js
+
 import React from "react";
 
 export default function App() {
-  return "App";
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
 }
